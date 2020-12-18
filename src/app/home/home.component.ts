4a275cfcebb449f2fdb8bdaffa343c0a7b806721
Anthony Cohen <>
2020-12-18 14:47:25 +0100

    refacto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,33 @@ import { ProductService } from './../services/product.service';
 })
 export class HomeComponent implements OnInit {
   productsList: Product[];
+  advantages = [
+    {
+      title: 'home.advantages.money-gain.title',
+      icon: '../../assets/svg/saving.svg',
+      description: 'home.advantages.money-gain.description',
+    },
+    {
+      title: 'home.advantages.time-gain.title',
+      icon: '../../assets/svg/time.svg',
+      description: 'home.advantages.time-gain.description',
+    },
+    {
+      title: 'home.advantages.certified-sellers.title',
+      icon: '../../assets/svg/certified-sellers.svg',
+      description: 'home.advantages.certified-sellers.description',
+    },
+    {
+      title: 'home.advantages.secured-data.title',
+      icon: '../../assets/svg/privacy.svg',
+      description: 'home.advantages.secured-data.description',
+    },
+    {
+      title: 'home.advantages.advice-practitioners.title',
+      icon: '../../assets/svg/feedback.svg',
+      description: 'home.advantages.advice-practitioners.description',
+    },
+  ];
 
   constructor(private productService: ProductService) {}
 
